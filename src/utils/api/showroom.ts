df455e3ef2b4ee4b6e059371a9a7aaa93f3db4e7
Crytalenz <kelvinzzzzzzzzzz@gmail.com>
2024-04-05 23:41:14 +0700

    Update showroom.ts

--- src/utils/api/showroom.ts
+++ src/utils/api/showroom.ts
@@ -8,6 +8,7 @@ const ofetch = fetchCustom.create({
     _: new Date().getTime(),
   },
   headers: {
+    'Cookie': cookies,
     'Referer': 'https://www.showroom-live.com/',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
   },
