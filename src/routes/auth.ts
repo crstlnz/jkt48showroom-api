dd5889444370adc7427a75c33b6f5c6eae7422e9
Crytalenz <kelvinzzzzzzzzzz@gmail.com>
2024-07-25 11:55:46 +0700

    update

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -5,9 +5,9 @@ import { checkToken } from '@/utils/security/token'
 import { useCORS } from '@/utils/cors'
 
 const app = new Hono()
-app.use('*', useCORS('self'))
 app.use('*', checkToken(false))
 app.use('*', useShowroomSession())
+app.use('*', useCORS('self'))
 
 app.post('/login', ...login())
 app.post('/logout', ...logout())
